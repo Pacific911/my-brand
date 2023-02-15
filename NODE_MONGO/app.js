06f448fc0be43c86a02f0d4b088192bd93d7acbe
Pacific911 <nduwumwepacific@gmail.com>
2023-02-15 10:31:07 -0800

    no-edit

--- NODE_MONGO/app.js
+++ NODE_MONGO/app.js
@@ -48,8 +48,8 @@ app.set('view engine', 'ejs');
 
 database()
 
-app.listen(5000, () => {
-    console.log("Server has started!")
+app.listen(process.env.PORT, () => {
+    console.log('Server has started!');
 })
 
 module.exports = app
