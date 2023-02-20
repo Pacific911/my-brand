1f1d304cea17f008b37da7f783e6d40168b1d5a4
Pacific911 <nduwumwepacific@gmail.com>
2023-02-20 12:22:18 -0800

    new commit

--- NODE_MONGO/src/controllers/blogcontroller.js
+++ NODE_MONGO/src/controllers/blogcontroller.js
@@ -141,6 +141,8 @@ const login = async (req, res, Msg) => {
     if (aunthentication) {
       const token = generate(userLogin._id);
       res.cookie('jwt', token);
+      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
       res.status(200).json({
         code: 200,
         message: 'logged in successful',
