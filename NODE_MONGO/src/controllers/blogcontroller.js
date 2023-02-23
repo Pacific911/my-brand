c0bd1dfa627d2af2ef0b682fd1d8443a949cdd32
Pacific911 <nduwumwepacific@gmail.com>
2023-02-22 18:56:52 -0800

    made changes

--- NODE_MONGO/src/controllers/blogcontroller.js
+++ NODE_MONGO/src/controllers/blogcontroller.js
@@ -10,29 +10,29 @@ require('dotenv').config();
 //creating blogs
 
 const createBlog = async (req, res) => {
-  console.log(req.body)
-  const { blogname, image, blogdescription } = req.body;
-  const result = await cloudinary.uploader.upload(req.file.path, {
-    folder: 'blog/blogImage',
-    public_id: `${blogname}_cover`,
-  });
-  await blog .create({
-      blogname,
-      image: result.url,
-      blogdescription,
-    })
-    .then((done) => {
-      if (done) {
-        res.status(200).json({
-          code: 200,
-          success: true,
-          message: 'blog created successful',
-          blog: done,
-        });
-      }
-    }).catch ((err) =>{
-      res.json(err)
-    })
+  res.json(req.body)
+  // const { blogname, image, blogdescription } = req.body;
+  // const result = await cloudinary.uploader.upload(req.file.path, {
+  //   folder: 'blog/blogImage',
+  //   public_id: `${blogname}_cover`,
+  // });
+  // await blog .create({
+  //     blogname,
+  //     image: result.url,
+  //     blogdescription,
+  //   })
+  //   .then((done) => {
+  //     if (done) {
+  //       res.status(200).json({
+  //         code: 200,
+  //         success: true,
+  //         message: 'blog created successful',
+  //         blog: done,
+  //       });
+  //     }
+  //   }).catch ((err) =>{
+  //     res.json(err)
+  //   })
 };
 
 //viewing blogs
